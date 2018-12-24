ae338fab6af3c209845f25824d34f030fcf37b75
Siddharth <siddharthpadwal3@gmail.com>
2018-12-24 18:38:00 +0530

    adding js to run multiple color on click function

---
+++ index.js
@@ -0,0 +1,12 @@
+var color, i;
+
+function myFunction(){
+
+    color = ['red','purple','green','blue','pink','orange','brown','aqua'];
+
+    for (var i = 0; i < color.length; i++ ){
+        document.body.style.background = color;
+    }
+   
+}
+
